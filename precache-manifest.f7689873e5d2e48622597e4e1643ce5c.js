ece238ef8d352d58bb50a43f5911928aa2bfaa3b
肖晓倩 <xiaoxiaoqian@xiaoxiaoqiandeMacBook-Pro.local>
2020-07-14 20:29:55 +0800

    Updates

---
+++ precache-manifest.f7689873e5d2e48622597e4e1643ce5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dabeff7a742a9e13da0481d51aaddc15",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d5baa2994387e325e75",
+    "url": "/static/css/main.87d340bc.chunk.css"
+  },
+  {
+    "revision": "2aa248360e67dec9337d",
+    "url": "/static/js/2.d13ffb0e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d13ffb0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d5baa2994387e325e75",
+    "url": "/static/js/main.446a6d43.chunk.js"
+  },
+  {
+    "revision": "4626ff3ee96d693311cc",
+    "url": "/static/js/runtime-main.59bbe95c.js"
+  }
+]);
